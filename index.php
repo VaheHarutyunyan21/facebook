c944f41d104046bbc4eb7d69325340805cc07529
VaheHarutyunyan21 <vaheh@internet.ru>
2021-08-14 19:50:51 +0400

    New projectsdsdsd

---
+++ index.php
@@ -0,0 +1,127 @@
+<?php
+
+//    echo "hello world";
+//    print ('hello world');
+
+
+// $name = 'John';
+// echo $name;
+
+// int
+ $age = 29;
+// float
+ $height = 182.6;
+//String
+ $lastName = 'Doe';
+//array
+//object
+//null
+//resource
+//
+//$year = 2021;
+//
+//echo "</br>";
+//echo $year - $age;
+//echo "</br>";
+//echo $year + $age;
+//echo "</br>";
+//echo $year / $age;
+//echo "</br>";
+//echo $year * $age;
+
+//echo 5 > 4;
+//echo 5 < 4;
+//echo 5 <= 4;
+//echo 5 >= 4;
+//echo (5 + 6) * 2;
+
+$age = 29;
+//if ($age > 18) {
+//    echo 'You are allowed to watch';
+//}else{
+//    echo 'forbidden';
+//}
+
+//if ($age > 10 && $age < 20) {
+//    echo '10 - 20';
+//}elseif ($age < 50 && $age < 100) {
+//    echo '50 - 100';
+//}else {
+//    echo 'others';
+//}
+
+//$state = true;
+//if ($state) {
+//    echo 'ok';
+//}else {
+//    echo 'error';
+//}
+
+//if (true === 1) {
+//    echo 'true == 1';
+//}else {
+//    echo 'true !==1';
+//}
+
+
+//$name = 'John';
+//switch ($name) {
+//    case 'Doe':
+//        echo 'Doe';
+//        break;
+//    case 'John':
+//        echo 'John';
+//        break;
+//    case 'Menua':
+//        echo 'Menua';
+//        break;
+//    default:
+//        echo 'Not Found';
+//}
+
+//$age = -3;
+//switch ($age) {
+//    case $age > 10 && $age < 20:
+//        echo '10- 20';
+//        break;
+//    case $age > 1 && $age < 5:
+//        echo '1-5';
+//        break;
+//    case $age > -5 && $age <1:
+//        echo '-5 - 1';
+//        break;
+//    default:
+//        echo 'Not Found';
+//}
+
+//$name = 'John';
+
+//if ($name == 'John') {
+//    $userId = 5;
+//} else {
+//    $userId = 7;
+//}
+
+
+//echo $name == 'John' ? 5 :7;
+
+
+//$name = 'john';
+//echo $name;
+//
+//echo "</br>";
+//$name = 'saddsd';
+//echo $name;
+
+//define('NAME', 'John');
+//echo NAME;
+
+
+
+
+
+
+
+
+
+?>
